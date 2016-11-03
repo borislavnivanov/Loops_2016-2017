88b6af24c065ce3a3931ec39817da4319bc2e9dd
Borislav Nikolaev Ivanov <borislavnivanov@gmail.com>
2016-11-03 08:39:05 +0200

    update

--- BinaryToDecimal/BinaryToDecimal.cs
+++ BinaryToDecimal/BinaryToDecimal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Numerics;
 
 
 namespace BinaryToDecimal
@@ -8,7 +9,19 @@ namespace BinaryToDecimal
     {
         static void Main()
         {
-            
+            string input = Console.ReadLine();
+            string[] inputarray = Regex.Split(input, "");
+            BigInteger currentSum = 0;
+            BigInteger counter = 1;
+            BigInteger result = 0;
+
+            for (int i = inputarray.Length - 2; i > 0; i--)
+            {
+                currentSum = counter * int.Parse(inputarray[i]);
+                result = result + currentSum;
+                counter = counter * 2;
+            }
+            Console.WriteLine(result);
         }
     }
 }
--- DecimalToBinary/DecimalToBinary.cs
+++ DecimalToBinary/DecimalToBinary.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
+
 
 namespace DecimalToBinary
 {
     class DecimalToBinary
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
         }
     }
 }
